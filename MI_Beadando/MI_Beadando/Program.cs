81f3437762c23a2437f138d1a5b51bb47f37fc7a
ADMA <adrian.balogh@outlook.com>
2017-05-01 19:43:39 +0200

    c

--- MI_Beadando/MI_Beadando/Program.cs
+++ MI_Beadando/MI_Beadando/Program.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
+/// <summary>
+/// Minden állapot osztály õse.
+/// </summary>
 namespace MI_Beadando
 {
     #region abstract osztály
@@ -17,17 +19,48 @@ namespace MI_Beadando
         // Megvizsgálja, hogy a belsõ állapot célállapot-e.
         // Ha igen, akkor igazat ad vissza, egyébként hamisat.
         public abstract bool vegAllapotE();
+        // Klónoz. Azért van rá szükség, mert némelyik operátor hatását vissza kell vonnunk.
+        // A legegyszerûbb, hogy az állapotot leklónozom. Arra hívom az operátort.
+        // Ha gond van, akkor visszatérek az eredeti állapothoz.
+        // Ha nincs gond, akkor a klón lesz az állapot, amibõl folytatom a keresést.
+        // Ez sekély klónozást alkalmaz. Ha elég a sekély klónozás, akkor nem kell felülírni a gyermek osztályban.
+        // Ha mély klónozás kell, akkor mindenképp felülírandó.
         public abstract bool szuperOp(int i);
-        public abstract AbsztraktAllapot clone();
-        public abstract bool equals(AbsztraktAllapot a);
+        public virtual object Clone() { return MemberwiseClone(); }
+        // Csak akkor kell felülírni, ha emlékezetes backtracket akarunk használni, vagy mélységi keresést.
+        // Egyébként maradhat ez az alap implementáció.
+        // Programozás technikailag ez egy kampó metódus, amit az OCP megszegése nélkül írhatok felül.
+        public override bool Equals(Object a) { return false; }
+        // Ha két példány egyenlõ, akkor a hasítókódjuk is egyenlõ.
+        // Ezért, ha valaki felülírja az Equals metódust, ezt is illik felülírni.
+        public override int GetHashCode() { return base.GetHashCode(); }
     }
     #endregion
-    abstract class VakÁllapot : AbsztraktAllapot
+    abstract class VakAllapot : AbsztraktAllapot
     {
+        // Itt kell megadni azokat a mezõket, amelyek tartalmazzák a belsõ állapotot.
+        // Az operátorok belsõ állapot átmenetet hajtanak végre.
+        // Elõször az alapoperátorokat kell megírni.
+        // Minden operátornak van elõfeltétele.
+        // Minden operátor utófeltétele megegyezik az ÁllapotE predikátummal.
+        // Az operátor igazat ad vissza, ha alkalmazható, hamisat, ha nem alkalmazható.
+        // Egy operátor alkalmazható, ha a belsõ állapotra igaz
+        // az elõfeltétele és az állapotátmenet után igaz az utófeltétele.
+        // Ez az elsõ alapoperátor.
         private bool op1()
         {
+            // Ha az elõfeltétel hamis, akkor az operátor nem alkalmazható.
             if (!preOp1()) return false;
+            // állapot átmenet
+            //
+            // TODO: Itt kell kiegészíteni a kódot!
+            //
+            // Utófeltétel vizsgálata, ha igaz, akkor alkalmazható az operátor.
             if (vegAllapotE()) return true;
+            //
+            // TODO: Itt kell kiegészíteni a kódot!
+            //
+            // és vissza kell adni, hogy nem alkalmazható az operátor.
             return false;
         }
         private bool preOp1()
@@ -58,21 +91,27 @@ namespace MI_Beadando
         {
             switch (i)
             {
+                // Itt kell felsorolnom az összes alapoperátort.
+                // Ha egy új operátort veszek fel, akkor ide is fel kell venni.
                 case 0: return op1();
                 case 1: return op2();
+                // A paraméteres operátorok több alap operátornak megfelelnek, ezért itt több sor is tartozik hozzájuk.
+                // Hogy hány sor, az feladat függõ.
                 case 3: return op3(0);
                 case 4: return op3(1);
                 case 5: return op3(2);
                 default: return false;
             }
         }
+        // Visszaadja az alap operátorok számát.
         public override int getNumberOfOps()
         {
+            // Az utolsó case számát kell itt visszaadni.
+            // Ha bõvítjük az operátorok számát, ezt a számot is növelni kell.
             return 5;
         }
     }
-
-abstract class FerjekEsFelesegekAllapot: AbsztraktAllapot
+    class FerjekEsFelesegekAllapot : AbsztraktAllapot
     {
         int ferj; //férjek összesen
         int feleseg; //feleség összesen
@@ -93,25 +132,28 @@ abstract class FerjekEsFelesegekAllapot: AbsztraktAllapot
             felesegmasikpart = feleseg;
         }
         public override bool allapotE()
-        {
+        {// igaz, ha a szerzetesek biztonságban vannak
             return ((felesegegyikpart >= ferjegyikpart) || (felesegegyikpart == 0)) &&
                 ((felesegegyikpart >= ferjmasikpart) || (felesegmasikpart == 0));
         }
         public override bool vegAllapotE()
-        {
+        {// igaz, ha mindenki átért a jobb oldalra
             return ferjegyikpart == ferj && felesegegyikpart == feleseg;
         }
         private bool preOp(int feleseg, int ferj)
-        {
+        {        // A csónak 2 személyes, legalább egy ember kell az evezéshez.
+                 // Ezt végül is felesleges vizsgálni, mert a szuper operátor csak ennek megfelelõen hívja.
             if (ferj + feleseg > 2 || ferj + feleseg < 0 || ferj < 0 || feleseg < 0) return false;
             if (ferj == 'B')
                 return ferjegyikpart >= ferj && felesegegyikpart >= feleseg;
             else
                 return ferjmasikpart >= ferj && felesegmasikpart >= feleseg;
         }
+        // Átvisz a másik oldalra sz darab szerzetes és k darab kannibált.
         private bool Atvisz(int ferj, int feleseg)
-        {
+        { //elõfeltétel
             if (!preOp(feleseg, ferj)) return false;
+            //állapotváltzozás
             if (csonak == 'J')
             {
                 ferjegyikpart += ferj;
@@ -129,14 +171,17 @@ abstract class FerjekEsFelesegekAllapot: AbsztraktAllapot
                 ferjmasikpart += ferj;
                 felesegmasikpart += feleseg;
             }
+            //utófeltétel, megj.: az utófeltétel mindig megegyezik az ÁllapotE-vel.
             if (allapotE()) return true;
             return false;
         }
         public override int getNumberOfOps() { return 5; }
         public override bool szuperOp(int i)
         {
-           switch (i)
+            switch (i)
             {
+                // Itt kell felsorolnom az összes alapoperátort.
+                // Ha egy új operátort veszek fel, akkor ide is fel kell venni.
                 case 0: return Atvisz(1, 0);
                 case 1: return Atvisz(0, 1);
                 case 2: return Atvisz(1, 1);
@@ -154,6 +199,7 @@ abstract class FerjekEsFelesegekAllapot: AbsztraktAllapot
             FerjekEsFelesegekAllapot aa = (FerjekEsFelesegekAllapot)a;
             return aa.ferjegyikpart == ferjegyikpart && aa.felesegegyikpart == felesegegyikpart && aa.csonak == csonak;
         }
+        // Ha két példány egyenlõ, akkor a hasítókódjuk is egyenlõ.
         public override int GetHashCode()
         {
             return ferjegyikpart.GetHashCode() + felesegegyikpart.GetHashCode() + csonak.GetHashCode();
@@ -164,6 +210,11 @@ abstract class FerjekEsFelesegekAllapot: AbsztraktAllapot
         AbsztraktAllapot allapot;
         int melyseg;
         Csucs szulo;
+        // A szülõkön felfelé haladva a start csúcsig jutok.
+        // Konstruktor:
+        // A belsõ állapotot beállítja a start csúcsra.
+        // A hívó felelõssége, hogy a kezdõ állapottal hívja meg.
+        // A start csúcs mélysége 0, szülõje nincs.
 
         public Csucs(AbsztraktAllapot kezdoAllapot)
         {
@@ -189,14 +240,18 @@ abstract class FerjekEsFelesegekAllapot: AbsztraktAllapot
         }
         public override int GetHashCode() { return allapot.GetHashCode(); }
         public override String ToString() { return allapot.ToString(); }
-        public List<Csucs>Kiterjesztes()
+        // Alkalmazza az összes alkalmazható operátort.
+        // Visszaadja az így elõálló új csúcsokat.
+        public List<Csucs> Kiterjesztes()
         {
             List<Csucs> ujCsucsok = new List<Csucs>();
             for (int i = 0; i < getNumberOfOps(); i++)
             {
+                // Új gyermek csúcsot készítek.
                 Csucs ujCsucs = new Csucs(this);
-                if(ujCsucs.SzuperOperator(i))
-                {
+                // Kiprobálom az i.dik alapoperátort. Alkalmazható?
+                if (ujCsucs.SzuperOperator(i))
+                { // Ha igen, hozzáadom az újakhoz.
                     ujCsucsok.Add(ujCsucs);
                 }
             }
@@ -212,74 +267,127 @@ abstract class FerjekEsFelesegekAllapot: AbsztraktAllapot
         }
         protected Csucs GetStartCsucs() { return startCsucs; }
         public abstract Csucs Kereses();
-        public void megoldasKiirasa(Csucs egyTermialisCsucs)
+        /// <summary>
+        /// Kiíratja a megoldást egy terminális csúcs alapján.
+        /// Feltételezi, hogy a terminális csúcs szülõ referenciáján felfelé haladva eljutunk a start csúcshoz.
+        /// A csúcsok sorrendjét megfordítja, hogy helyesen tudja kiírni a megoldást.
+        /// Ha a csúcs null, akkor kiírja, hogy nincs megoldás.
+        /// </summary>
+        /// <param name="egyTerminálisCsúcs">
+        /// A megoldást képviselõ terminális csúcs vagy null.
+        /// </param>
+        public void megoldasKiirasa(Csucs egyTerminalisCsucs)
         {
-            if (egyTermialisCsucs == null)
+            if (egyTerminalisCsucs == null)
             {
                 Console.WriteLine("Nincs megoldás!");
                 return;
             }
+            // Meg kell fordítani a csúcsok sorrendjét.
             Stack<Csucs> megoldas = new Stack<Csucs>();
-            Csucs aktCsucs = egyTermialisCsucs;
+            Csucs aktCsucs = egyTerminalisCsucs;
             while (aktCsucs != null)
             {
                 megoldas.Push(aktCsucs);
                 aktCsucs = aktCsucs.GetSzulo();
             }
+            // Megfordítottuk, lehet kiírni.
             foreach (Csucs akt in megoldas) Console.WriteLine(akt);
         }
     }
+    /// <summary>
+    /// A backtrack gráfkeresõ algoritmust megvalósító osztály.
+    /// A három alap backtrack algoritmust egyben tartalmazza. Ezek
+    /// - az alap backtrack
+    /// - mélységi korlátos backtrack
+    /// - emlékezetes backtrack
+    /// Az ág-korlátos backtrack nincs megvalósítva.
+    /// </summary>
     class BackTrack : Grafkereso
     {
-        int korlat;
-        bool emlekezetes;
-        public BackTrack(Csucs startCsucs, int korlat,bool emlekezetes)
-        :base(startCsucs)
+        int korlat; // Ha nem nulla, akkor mélységi korlátos keresõ.
+        bool emlekezetes; // Ha igaz, emlékezetes keresõ.
+        public BackTrack(Csucs startCsucs, int korlat, bool emlekezetes)
+        : base(startCsucs)
         {
             this.korlat = korlat;
             this.emlekezetes = emlekezetes;
         }
+        // nincs mélységi korlát, se emlékezet
         public BackTrack(Csucs startCsucs) : this(startCsucs, 0, false) { }
+        // mélységi korlátos keresõ
         public BackTrack(Csucs startCsucs, int korlat) : this(startCsucs, korlat, false) { }
+        // emlékezetes keresõ
         public BackTrack(Csucs startCsucs, bool emlekezetes) : this(startCsucs, 0, emlekezetes) { }
+        // A keresés a start csúcsból indul.
+        // Egy terminális csúcsot ad vissza. A start csúcsból el lehet jutni ebbe a terminális csúcsba.
+        // Ha nincs ilyen, akkor null értéket ad vissza.
         public override Csucs Kereses() { return Kereses(GetStartCsucs()); }
+        // A keresõ algoritmus rekurzív megvalósítása.
+        // Mivel rekurzív, ezért a visszalépésnek a "return null" felel meg.
         private Csucs Kereses(Csucs aktCsucs)
         {
             int melyseg = aktCsucs.GetMelyseg();
+            // mélységi korlát vizsgálata
             if (korlat > 0 && melyseg >= korlat) return null;
+            // emlékezet használata kör kiszûréséhez
             Csucs aktSzulo = null;
             if (emlekezetes) aktSzulo = aktCsucs.GetSzulo();
-            while(aktSzulo != null)
+            while (aktSzulo != null)
             {
-                if(aktCsucs.TerminalisCsucsE())
+                // Ellenõrzöm, hogy jártam-e ebben az állapotban. Ha igen, akkor visszalépés.
+                if (aktCsucs.Equals(aktSzulo)) return null;
+                // Visszafelé haladás a szülõi láncon.
+                aktSzulo = aktSzulo.GetSzulo();
+            }
+            if (aktCsucs.TerminalisCsucsE())
+            {
+                // Megvan a megoldás, vissza kell adni a terminális csúcsot.
+                return aktCsucs;
+            }
+            // Itt hívogatom az alapoperátorokat a szuper operátoron
+            // keresztül. Ha valamelyik alkalmazható, akkor új csúcsot
+            // készítek, és meghívom önmagamat rekurzívan.
+            for (int i = 0; i < aktCsucs.getNumberOfOps(); i++)
+            {
+                // Elkészítem az új gyermek csúcsot.
+                // Ez csak akkor lesz kész, ha alkalmazok rá egy alkalmazható operátort is.
+                Csucs ujCsucs = new Csucs(aktCsucs);
+                if (ujCsucs.SzuperOperator(i))
                 {
-                    return aktCsucs;
-                }
-                for (int i = 0; i < aktCsucs.getNumberOfOps(); i++)
-                {
-                    Csucs ujCsucs = new Csucs(aktCsucs);
-                    if(ujCsucs.SzuperOperator(i))
+                    // Ha igen, rekurzívan meghívni önmagam az új csúcsra.
+                    // Ha nem null értéket ad vissza, akkor megvan a megoldás.
+                    // Ha null értéket, akkor ki kell próbálni a következõ alapoperátort.
+                    Csucs terminalis = Kereses(ujCsucs);
+                    if (terminalis != null)
+                    // Visszaadom a megoldást képviselõ terminális csúcsot.!= null)
                     {
-                        Csucs terminalis = Kereses(ujCsucs);
-                        if(terminalis != null)
-                        {
-                            return terminalis;
-                        }
+                        return terminalis;
                     }
-                }  
+                    // Az else ágon kellene visszavonni az operátort.
+                    // Erre akkor van szükség, ha az új gyermeket létrehozásában nem lenne klónozást.
+                    // Mivel klónoztam, ezért ez a rész üres.
+                }
             }
             return null;
         }
-
     }
+    /// <summary>
+    /// Mélységi keresést megvalósító gráfkeresõ osztály.
+    /// Ez a megvalósítása a mélységi keresésnek felteszi, hogy a start csúcs nem terminális csúcs.
+    /// A nyílt csúcsokat veremben tárolja.
+    /// </summary>
     class MelysegiKereses : Grafkereso
     {
+        // Mélységi keresésnél érdemes a nyílt csúcsokat veremben tárolni,
+        // mert így mindig a legnagyobb mélységû csúcs lesz a verem tetején.
+        // Így nem kell külön keresni a legnagyobb mélységû nyílt csúcsot, amit ki kell terjeszteni.
         Stack<Csucs> Nyilt; // Nílt csúcsok halmaza.
         List<Csucs> Zart; // Zárt csúcsok halmaza.
         bool korFigyeles; // Ha hamis, végtelen ciklusba eshet.
         public MelysegiKereses(Csucs startCsucs, bool korFigyeles) :
         base(startCsucs)
-    {
+        {
             Nyilt = new Stack<Csucs>();
             Nyilt.Push(startCsucs); // kezdetben csak a start csúcs nyílt
             Zart = new List<Csucs>(); // kezdetben a zárt csúcsok halmaza üres
@@ -336,11 +444,22 @@ abstract class FerjekEsFelesegekAllapot: AbsztraktAllapot
             return null;
         }
     }
-}
-class Program
-{
-    static void Main(string[] args)
+    class Program
     {
+        static void Main(string[] args)
+        {
+            Csucs startCsucs;
+            Grafkereso kereso;
+            Grafkereso kereso2;
+            Console.WriteLine("A 2.37-es feladat megoldása");
+            startCsucs = new Csucs(new FerjekEsFelesegekAllapot(10, 10));
+            Console.WriteLine("A kereső egy mélységi korlátos és emlékezetes backtrack");
+            kereso = new BackTrack(startCsucs, 10, true);
+            kereso.megoldasKiirasa(kereso.Kereses());
+            kereso2 = new MelysegiKereses(startCsucs, true);
+            kereso2.megoldasKiirasa(kereso2.Kereses());
+            Console.ReadLine();
+        }
     }
 }
 
